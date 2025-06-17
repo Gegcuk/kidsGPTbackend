70e87eb47fcfe75b0f7d09eb7eea06360f1ac6c0
Aleksei Lazunin <gegcuk@gmail.com>
2025-06-17 13:34:30 +0100

    Init configs

--- src/main/java/uk/gegc/kidsgptbackend/KidsGpTbackendApplication.java
+++ src/main/java/uk/gegc/kidsgptbackend/KidsGpTbackendApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class KidsGpTbackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(KidsGpTbackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(KidsGpTbackendApplication.class, args);
+    }
 
 }
--- src/test/java/uk/gegc/kidsgptbackend/KidsGpTbackendApplicationTests.java
+++ src/test/java/uk/gegc/kidsgptbackend/KidsGpTbackendApplicationTests.java
@@ -1,13 +1,31 @@
 package uk.gegc.kidsgptbackend;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class KidsGpTbackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private DataSource dataSource;
 
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void databaseConnectionIsValid() throws Exception {
+        try (Connection connection = dataSource.getConnection()) {
+            assertThat(connection).isNotNull();
+            assertThat(connection.isValid(1)).isTrue();
+        }
+    }
 }
