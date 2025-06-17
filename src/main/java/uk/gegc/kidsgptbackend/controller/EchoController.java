e33bc3a94b104e9c0d19f64b718555b7c824887d
Aleksei Lazunin <gegcuk@gmail.com>
2025-06-17 23:01:35 +0100

    Test echo endpoint

---
+++ src/main/java/uk/gegc/kidsgptbackend/controller/EchoController.java
@@ -0,0 +1,15 @@
+package uk.gegc.kidsgptbackend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EchoController {
+
+    @GetMapping("/echo")
+    public String echo(@RequestParam(value = "msg", defaultValue = "Hello") String message){
+        return message;
+    }
+
+}
