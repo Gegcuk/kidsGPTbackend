4cdf916a209c88b5949c25f03f3f4d8bbd181ddb
Aleksei Lazunin <gegcuk@gmail.com>
2025-06-17 12:59:41 +0100

    Initial commit

---
+++ src/main/java/uk/gegc/kidsgptbackend/KidsGpTbackendApplication.java
@@ -0,0 +1,13 @@
+package uk.gegc.kidsgptbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KidsGpTbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KidsGpTbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uk/gegc/kidsgptbackend/KidsGpTbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.gegc.kidsgptbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KidsGpTbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
