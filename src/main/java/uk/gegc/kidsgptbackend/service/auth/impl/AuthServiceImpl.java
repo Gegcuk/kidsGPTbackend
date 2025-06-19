8e6dea0fc460dbf4f07f0f50ca3cf21b1f1e44d6
Aleksei Lazunin <gegcuk@gmail.com>
2025-06-19 19:48:01 +0100

    Add transactional(read-only) in AuthServiceImpl getProfile(method) to avoid internal server bug

--- src/main/java/uk/gegc/kidsgptbackend/service/auth/impl/AuthServiceImpl.java
+++ src/main/java/uk/gegc/kidsgptbackend/service/auth/impl/AuthServiceImpl.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -97,6 +98,7 @@ public class AuthServiceImpl implements AuthService {
     }
 
     @Override
+    @Transactional(readOnly = true)
     public UserProfileDto getProfile(String username) {
         User user = userRepository.findByUsername(username)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found"));
