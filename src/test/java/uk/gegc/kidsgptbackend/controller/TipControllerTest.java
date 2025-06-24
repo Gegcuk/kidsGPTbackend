554acfdc8c33ee6e0376b1334c36487d9fec41fc
Aleksei Lazunin <gegcuk@gmail.com>
2025-06-24 22:16:29 +0100

    Change MockBean to MockitoBean

--- src/test/java/uk/gegc/kidsgptbackend/controller/TipControllerTest.java
+++ src/test/java/uk/gegc/kidsgptbackend/controller/TipControllerTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import uk.gegc.kidsgptbackend.dto.tips.DailyTipDto;
 import uk.gegc.kidsgptbackend.service.tips.DailyTipService;
@@ -22,7 +23,7 @@ class TipControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     private DailyTipService dailyTipService;
 
     @Test
