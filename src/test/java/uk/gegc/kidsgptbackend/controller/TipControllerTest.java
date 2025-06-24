e27c0e373ac25653fe8680b12ff8f429e806561c
Aleksei Lazunin <gegcuk@gmail.com>
2025-06-24 22:21:11 +0100

    Add @ActiveProfiles("test")

--- src/test/java/uk/gegc/kidsgptbackend/controller/TipControllerTest.java
+++ src/test/java/uk/gegc/kidsgptbackend/controller/TipControllerTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import uk.gegc.kidsgptbackend.dto.tips.DailyTipDto;
@@ -17,6 +17,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
+@ActiveProfiles("test")
 @AutoConfigureMockMvc
 class TipControllerTest {
 
